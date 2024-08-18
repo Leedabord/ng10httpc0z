fb6f7229bf3e31b82ec0d9ebe71cf5aab9d199fc
Leedabord <gwforumlakers@gmail.com>
2024-08-17 23:52:05 -0700

    1 file modified

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -31,14 +31,17 @@ this.courses$ = this.http
         "/courses.json",
         {headers})
    * 
+
+        curl "https://api.airtable.com/v0/app0hohtq4b1nM0Kb/AFToscano?maxRecords=3&view=Main%20View" \
+  -H "Authorization: Bearer YOUR_SECRET_API_TOKEN"
    */
   getAllIssues(): void {
     // const API_URL2 = 'https://api.airtable.com/v0/app0hohtq4b1nM0Kb/Issues' 
-    const API_URL2 = 'https://airtable.com/app0hohtq4b1nM0Kb/tbleP0LrB6Hf9ysFS';
+    const API_URL2 = 'https://airtable.com/app0hohtq4b1nM0Kb/Issues';
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        'Authentication': 'Bearer patgbCJgQnURgkXpI.f72c7c10a614e68e2ba92c6e7a437e64312719fe9ad7f7c38b05164dfe445a32'  
+        'Authorization': 'Bearer patgbCJgQnURgkXpI.f72c7c10a614e68e2ba92c6e7a437e64312719fe9ad7f7c38b05164dfe445a32'  
       //  'api_key': 'key66fQg5IghIIQmb'  
        // 'x-apikey': '5821f61550e9b39131fe1b6f'  
         // 569a2b87566759cf4b984a50'  // 5821f61550e9b39131fe1b6f
